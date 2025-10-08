1c5102f915dfec8656976fc18a595eeac8500bcc
Heloísa Nogueira <heloisancc@al.insper.edu.br>
2025-10-08 15:20:37 -0300

    pc.vhd

--- d_logSeq/src/pc.vhd
+++ d_logSeq/src/pc.vhd
@@ -66,52 +66,6 @@ architecture arch of PC is
 
 begin
 
-  -- 1 Inc16: inc_out = outputReg + 1
-  INC : Inc16
-    port map(
-      a => outputReg,
-      q => inc_out
-    );
-
-  -- 2 Mux de incremento: after_inc = (increment='1') ? inc_out : outputReg
-  MUX_INC : Mux16
-    port map(
-      a   => outputReg,
-      b   => inc_out,
-      sel => increment,
-      q   => after_inc
-    );
-
-  -- 3 Mux de load: after_load = (load='1') ? input : after_inc
-  MUX_LOAD : Mux16
-    port map(
-      a   => after_inc,
-      b   => input,
-      sel => load,
-      q   => after_load
-    );
-
-  -- 4 Mux de reset: next_value = (reset='1') ? ZERO16 : after_load
-  MUX_RESET : Mux16
-    port map(
-      a   => after_load,
-      b   => ZERO16,
-      sel => reset,
-      q   => next_value
-    );
-
-  -- 5 Registrador: carrega next_value a cada clock.
-  --    Aqui assumimos que o Register16 aceita load='1' para sempre carregar.
-  REG : Register16
-    port map(
-      clock  => clock,
-      input  => next_value,
-      load   => '1',
-      output => outputReg
-    );
-
-  -- Saída do PC
-  output <= outputReg;
 
 end architecture;
 
